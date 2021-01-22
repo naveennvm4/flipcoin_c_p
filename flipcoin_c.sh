f7a8abc0768b04a870e12c6916c2287956731437
naveennvm4 <naveennvm4@gmail.com>
2021-01-22 18:17:32 +0530

    store the double T combinations

--- flipcoin_c.sh
+++ flipcoin_c.sh
@@ -2,9 +2,9 @@ spots=2
 coin=0
 if [ coin=$RANDOM%$spots+1 ]
 then
-    echo "Heads"
+    echo "it is heads"
 else
-    echo "Tales"
+    echo "it is tales"
 fi
 function coinFlip() {
 temp=$1
@@ -13,6 +13,7 @@ twin=0
 for (( i=0 ; $i < $temp ; i++ ))
 do
 rand=$(($RANDOM%2))
+
 if [ $rand  -eq 1 ]
 then 
 	echo "Head Wins"
@@ -38,4 +39,49 @@ do
 	echo $KEY
   echo "Value: ${arr[$KEY]}"
 done
+}
+function coinDoublet() 
+{
+temp=$1
+HHwin=0
+HTwin=0
+THwin=0
+TTwin=0
+for (( i=0 ; $i < $temp ; i++ ))
+do
+rand=$(($RANDOM%2))
+rand1=$(($RANDOM%2))
+if [ $rand -eq 1 -a $rand1 -eq 1 ]
+then
+	echo "HH wins"
+	HHwin=$(( $HHwin + 1))
+elif [ $rand -eq 1 -a $rand1 -eq 0 ]
+then
+        echo "HT wins"
+        HTwin=$(( $HTwin + 1))
+elif [ $rand -eq 0 -a $rand1 -eq 1 ]
+then
+        echo "TH wins"
+        THwin=$(( $THwin + 1))
+elif [ $rand -eq 0 -a $rand1 -eq 0 ]
+then
+        echo "TT wins"
+        TTwin=$(( $TTwin + 1))
+else 
+	break
+fi
+done
+echo $HHwin
+x=$(($HHwin/$temp))
+HHpercentage=$(( $x * 100 ))
+echo $HTwin
+y=$(($HTwin/$temp))
+HTpercentage=$(( $y * 100 ))
+echo $THwin
+z=$(($THwin/$temp))
+THpercentage=$(( $ * 100 ))
+echo $TTwin
+d=$(($hwin/$temp))
+TTpercentage=$(( $d * 100 ))
+arr=( $HHpercentage $HTpercentage $THpercentage $TTpercentge )
 }
